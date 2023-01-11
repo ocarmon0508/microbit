0a70ce4621ee666b00c980e3b47540a1a0f34e83
ocarmon0508 <118901512+ocarmon0508@users.noreply.github.com>
2023-01-11 13:42:59 +0100

    Update bucle.py

--- bucle.py
+++ bucle.py
@@ -14,5 +14,12 @@ for i in range (101, 1, -1):
 from microbit import *
 #para contar de 5 en 5 usando for. 
 for i in range (1, 101, 5)
-     display.scroll(str(i))
+    display.scroll(str(i))
+     (while) 
+    # Imports go at the top
+from microbit import *
+i=1
+while(i<31):
+    display.scroll(str(i))
+    i=i+5
 
