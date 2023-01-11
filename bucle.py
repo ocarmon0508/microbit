81c6e698b2d6105e8330499b5bda12fb8617f20d
ocarmon0508 <118901512+ocarmon0508@users.noreply.github.com>
2023-01-11 13:37:12 +0100

    Update bucle.py

--- bucle.py
+++ bucle.py
@@ -1,5 +1,18 @@
-# Imports go at the top
+(from) # Imports go at the top
 from microbit import *
 
 for i in range(1, 101):
     display.scroll(str(i))
+ (while)   
+# Imports go at the top
+from microbit import *
+
+#invertido. para ir desde 101 hasta 1. 
+for i in range (101, 1, -1):
+    display.scroll(str(i))
+    
+from microbit import *
+#para contar de 5 en 5 usando for. 
+for i in range (1, 101, 5)
+     display.scroll(str(i))
+
