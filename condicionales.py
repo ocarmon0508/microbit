3d54837022b2a34358e035941b72f54b2101cff2
ocarmon0508 <118901512+ocarmon0508@users.noreply.github.com>
2023-01-16 13:00:42 +0100

    Update condicionales.py

--- condicionales.py
+++ condicionales.py
@@ -1,1 +1,13 @@
+from microbit import *
+while True:
+    if button_a.is_pressed() and button_b.is_pressed():
+        display.scroll("AB")
+        display.scroll("Adios!",delay=80)
+        break
+    elif button_a.is_pressed():
+        display.scroll("A")
+    elif button_a.is_pressed():
+        display.scroll("B")
+    else:
+        display.show("?")
 
