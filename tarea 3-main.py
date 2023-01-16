bfc7eef6a619356a262cd352928f9c02429ac05b
ocarmon0508 <118901512+ocarmon0508@users.noreply.github.com>
2023-01-16 13:35:49 +0100

    Add files via upload

---
+++ tarea 3-main.py
@@ -0,0 +1,13 @@
+# Imports go at the top
+from microbit import *
+contador = 0 
+
+
+while True:
+    if button_a.get_presses():
+        contador = contador + 1
+    elif button_b.get_presses():
+        contador = contador -1
+    else:
+         display.show(contador)
+
